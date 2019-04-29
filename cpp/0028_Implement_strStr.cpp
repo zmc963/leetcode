dd1de9bca2c3cd86b0096b7f66167334dbf0d9d2
layka <zmc963@gmail.com>
2019-04-30 01:28:36 +0800

    update 0028 with KMP algo

--- cpp/0028_Implement_strStr.cpp
+++ cpp/0028_Implement_strStr.cpp
@@ -1,59 +1,68 @@
-class Solution {
+class Solution
+{
+    using SIZE_TYPE = std::string::size_type;
+
 public:
     int strStr(string haystack, string needle)
     {
-        if( needle.empty()  )
+        if( needle.empty() ) return 0;
+
+        const auto CHARACER_SET_SIZE = 256; // ASCII code
+        const auto N_SIZE = needle.size();
+        std::vector<std::vector<SIZE_TYPE>> dfa( CHARACER_SET_SIZE, std::vector<SIZE_TYPE>( N_SIZE, 0 ) );
+
+        dfa.at(needle.at(0)).at(0) = 1;
+        for( SIZE_TYPE state=1, prev_state=0; state<N_SIZE; ++state )
         {
-            return 0;
+            for( SIZE_TYPE i=0; i<CHARACER_SET_SIZE; ++i )
+            {
+                dfa.at(i).at(state) = dfa.at(i).at(prev_state);
+            }
+
+            auto next_c = needle.at(state);
+            dfa.at(next_c).at(state) = state+1;
+
+            prev_state = dfa.at(next_c).at(prev_state);
         }
 
         const auto H_SIZE = haystack.size();
-        const auto N_SIZE = needle.size();
-        if( H_SIZE < N_SIZE )
+        for( SIZE_TYPE i=0, state=0; i<H_SIZE; ++i )
         {
-            return -1;
-        }
+            auto next_c = haystack.at(i);
+            state = dfa.at(next_c).at(state);
 
-        using IDX_TYPE = std::string::size_type;
-        for( IDX_TYPE i=0; i<=H_SIZE-N_SIZE; ++i )
-        {
-            IDX_TYPE t_j = 0;
-            while( (t_j<N_SIZE) && (haystack.at(t_j+i) == needle.at(t_j)) )
-            {
-                ++t_j;
-            }
-
-            if( N_SIZE == t_j )
-            {
-                return i;
-            }
-
-
-
-
-//             if( haystack.at(i) == needle.front() )
-//             {
-//                 if( H_SIZE-i < N_SIZE )
-//                 {
-//                     return -1;
-//                 }
-
-//                 std::string::size_type j = 1;
-//                 for( ; j<N_SIZE; ++j )
-//                 {
-//                     if( haystack.at(i+j) != needle.at(j) )
-//                     {
-//                         break;
-//                     }
-//                 }
-
-//                 if( j == N_SIZE )
-//                 {
-//                     return i;
-//                 }
-//             }
+            if( N_SIZE == state ) return i+1-N_SIZE;
         }
 
         return -1;
+
+//         if( needle.empty()  )
+//         {
+//             return 0;
+//         }
+
+//         const auto H_SIZE = haystack.size();
+//         const auto N_SIZE = needle.size();
+//         if( H_SIZE < N_SIZE )
+//         {
+//             return -1;
+//         }
+
+//         using IDX_TYPE = std::string::size_type;
+//         for( IDX_TYPE i=0; i<=H_SIZE-N_SIZE; ++i )
+//         {
+//             IDX_TYPE t_j = 0;
+//             while( (t_j<N_SIZE) && (haystack.at(t_j+i) == needle.at(t_j)) )
+//             {
+//                 ++t_j;
+//             }
+
+//             if( N_SIZE == t_j )
+//             {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
     }
 };
